95ceceb400c042f2d9c68041764d5e9ceeb20761
Tyler Hurley <tylerjohnhurley@gmail.com>
2021-10-10 12:31:42 -0400

    basic nav bar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Sign Up</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
+
+
+
